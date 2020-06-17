5d65cb89c40d44e7d9e27f4f82e9dfe7d1ae9192
Vitor Gabriel Moura de Melo <65458418+vitorgabrielmoura@users.noreply.github.com>
2020-06-17 13:24:12 -0300

    Update ex28

--- Linguagem C/Ex28.c
+++ Linguagem C/Ex28.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Crie uma função capaz de criar a transposta de uma matriz. */
+/* Crie uma funÃ§Ã£o capaz de criar a transposta de uma matriz. */
 
-void transporMatriz(int matriz[3][3]){
-	int l, c, matriztransposta[3][3];	
+void transporMatriz(int matriz[][3], int n){
+	int l, c;	
 	
-	for(l=0; l< 3; l++){
+	for(l=0; l< n; l++){
 		for(c=0; c< 3; c++){
 			printf("Digite um numero para a matriz na linha %d e coluna %d ", l, c);
 			scanf("%d", &matriz[l][c]);
@@ -14,7 +14,7 @@ void transporMatriz(int matriz[3][3]){
 	}
 
 	printf("\nMATRIZ TRANSPOSTA\n\n");
-	for(l=0; l< 3; l++){
+	for(l=0; l< n; l++){
 		for(c=0; c< 3; c++){
 			matriztransposta[l][c] = matriz[c][l];
 			printf("%d  ", matriztransposta[l][c]);
@@ -26,7 +26,7 @@ void transporMatriz(int matriz[3][3]){
 int main(int argc, char *argv[]) {
 	int matriz[3][3];
 	
-	transporMatriz(matriz);
+	transporMatriz(matriz, 3);
 	
 
 	return 0;
